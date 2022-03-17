d83203e48cf419a48cf6705feac00b2a21a1d7a5
Tamim Jabr <tj222kg@student.lnu.se>
2022-03-17 22:17:16 +0100

    add students to db

---
+++ src/main/java/com/example/API/Spring/Boot/student/StudentConfig.java
@@ -0,0 +1,22 @@
+package com.example.API.Spring.Boot.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+          Student tamim = new Student( "Tamim", 26,"tamim.se@hotmail.com", LocalDate.of(1995,8,9));
+          Student alex= new Student( "Alex", 25,"alex@example.com", LocalDate.of(2000,12,9));
+
+          repository.saveAll(List.of(tamim,alex));
+    };
+    }
+}
--- src/main/java/com/example/API/Spring/Boot/student/StudentService.java
+++ src/main/java/com/example/API/Spring/Boot/student/StudentService.java
@@ -17,8 +17,6 @@ public class StudentService {
     }
 
     public List<Student> getStudents() {
-        Student  student1 = new Student(1L, "Tamim",26, "example@hotmail.com", LocalDate.of(1995,8,9));
-        Student  student2 = new Student(2L, "Nataly",26, "example2@hotmail.com", LocalDate.of(1995,2,02));
-        return List.of(student1, student2);
+        return studentRepository.findAll();
     }
 }
