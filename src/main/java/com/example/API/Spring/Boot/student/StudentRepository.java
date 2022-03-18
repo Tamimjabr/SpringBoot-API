aca3d9bb118c012013472bb4f56971aee2e60833
Tamim Jabr <tj222kg@student.lnu.se>
2022-03-18 13:28:07 +0100

    check if the user already exists in db before adding

--- src/main/java/com/example/API/Spring/Boot/student/StudentRepository.java
+++ src/main/java/com/example/API/Spring/Boot/student/StudentRepository.java
@@ -1,10 +1,15 @@
 package com.example.API.Spring.Boot.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 // <The type, the key>
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
+    @Query("select s from Student s where s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
 }
--- src/main/java/com/example/API/Spring/Boot/student/StudentService.java
+++ src/main/java/com/example/API/Spring/Boot/student/StudentService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 // to be able to inject StudentService into controller
 @Service
@@ -22,6 +23,14 @@ public class StudentService {
 
     public void addStudent(Student student){
         // studentRepository.save(student);
-        System.out.println(student);
+        Optional<Student> existedStudent =this.studentRepository.findStudentByEmail(student.getEmail());
+        System.out.println(existedStudent);
+        System.out.println(existedStudent.isPresent());
+        if(existedStudent.isPresent()){
+            throw new IllegalStateException("Student with email: "+student.getEmail()+" already exists");
+        }
+        this.studentRepository.save(student);
+
+
     }
 }
