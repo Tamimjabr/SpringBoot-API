853f507134b6450bee087e07f9cae7a8eb6a1c23
Tamim Jabr <tj222kg@student.lnu.se>
2022-03-20 14:32:32 +0100

    add exception handler

---
+++ src/main/java/com/example/API/Spring/Boot/exception/ApiExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.example.API.Spring.Boot.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@ControllerAdvice // saying that the class is used for exception handling
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(value = {ApiRequestException.class})
+    public ResponseEntity<Object> handleApiRequestException(ApiRequestException e) {
+        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+        ApiException apiException=  new ApiException(
+            e.getMessage(),
+            e,
+            badRequest,
+            ZonedDateTime.now(ZoneId.of("Z"))
+        );
+        return new ResponseEntity<>(apiException, badRequest);
+    }
+}
